c987fbbc955e857892027990405b0571c864f566
Fpsska <fpsska1337@gmail.com>
2023-02-18 14:48:57 +0300

    fix: syntax by new prettier, eslint rules

--- src/assets/js/getUsers.js
+++ src/assets/js/getUsers.js
@@ -1,17 +1,17 @@
 export const getUsers = async () => {
-  try {
-    const id = 1
-    const URL = `https://jsonplaceholder.typicode.com/users/${id}`
+    try {
+        const id = 1;
+        const URL = `https://jsonplaceholder.typicode.com/users/${id}`;
 
-    const response = await fetch(URL)
+        const response = await fetch(URL);
 
-    if (!response.ok) {
-      throw new Error('some erorr with response')
+        if (!response.ok) {
+            throw new Error('some erorr with response');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err.message || err);
     }
-
-    const data = await response.json()
-    return data
-  } catch (err) {
-    console.error(err.message || err)
-  }
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import './index.html'
-import './assets/styles/style.scss'
+import './index.html';
+import './assets/styles/style.scss';
 
-import { getUsers } from './assets/js/getUsers'
+import { getUsers } from './assets/js/getUsers';
 
-import image from './assets/images/image-3.jpg'
+import image from './assets/images/image-3.jpg';
 
 // /. imports
 
-console.log('Hello World!')
-console.log(getUsers())
+console.log('Hello World!');
+console.log(getUsers());
 
-const box = document.querySelector('.box')
+const box = document.querySelector('.box');
 
-const imageElement = new Image()
-imageElement.src = image
-imageElement.alt = 'nature image'
-imageElement.width = 500
-imageElement.height = 400
+const imageElement = new Image();
+imageElement.src = image;
+imageElement.alt = 'nature image';
+imageElement.width = 500;
+imageElement.height = 400;
 
-box.append(imageElement)
+box.append(imageElement);
